4ce8839b2f8f9cd6964630d3083274f56d699e3a
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-01-07 00:08:21 +0530

    Add character counter, app.jsx and main.jsx file

---
+++ basic-projects/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SimpleCalculator from './components/SimpleCalculator'
+
+const App = () => {
+  return (
+    <div>
+      <SimpleCalculator/>
+    </div>
+  )
+}
+
+export default App
